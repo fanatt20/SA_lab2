7049e7f0a16f81e16a26a980e20c3013d9ccb3d7
Andrey Nikitin <andrii.d.nikitin@gmail.com>
2015-10-22 14:05:54 +0300

    Added class for input data normalization .

---
+++ SA_lab1/src/Algorithms/DataNormalizer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class DataNormalizer
+    {
+        private static double DELTA = 0.000000001;
+        public static double[][] Normalize(double[][] rawData)
+        {
+            if (rawData == null)
+                return null;
+            double[][] result = (double[][])rawData.Clone();
+            int rows = rawData.GetLength(0);
+            int columns = rawData.GetLength(1);
+            if (rows == 0 || columns == 0)
+                return rawData;
+            for (int i = 0; i < columns; i++)
+            {
+                result[i] = NormalizeColumn(rawData[i]);
+            }
+            return result;
+        }
+
+        private static double[] NormalizeColumn(double[] array)
+        {
+            double[] result = (double[])array.Clone();
+            double min = array.Min();
+            double max = array.Max();
+            int i = 0;
+            double d = (max - min);
+            if (IsDoubleValueZero(d))
+                foreach (double value in array)
+                {
+                    result[i] = value;
+                }
+            else
+                foreach (double value in array)
+                {
+                    result[i] = (value - min) / d;
+                }
+            return result;
+        }
+        private static bool IsDoubleValueZero(double value)
+        {
+            return (-DELTA < value) && (value < DELTA);
+        }
+    }
+}
