512341af6086e9c21abf8daacf92790f4574ac3e
Andrey Venzhyk <fanatt20@gmail.com>
2015-11-01 12:40:51 +0200

    add Log.Write(doub[][])

--- SA_lab1/src/UI/Log.cs
+++ SA_lab1/src/UI/Log.cs
@@ -1,16 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace UI
 {
     public class Log
     {
         public static TextBoxBase Target { get; set; }
-        public static void Write(String str) { Target.Text += str;}
-        public static void WriteLine(String str) { Write("\n" + str); }
+
+        public static void Write(string str)
+        {
+            Target.Text += str;
+        }
+
+        public static void WriteLine(string str)
+        {
+            Write("\n" + str);
+        }
+
+        public static void Write(double[][] matrix)
+        {
+            foreach (var array in matrix)
+            {
+                foreach (var element in array)
+                {
+                    Write(element + "\t");
+                }
+                WriteLine("");
+            }
+        }
     }
 }
